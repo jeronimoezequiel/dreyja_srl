9c811a0e217500ca31df975807935dfbd599ea48
jeronimo_nuñez <jeronimonunez96@gmail.com>
2023-12-05 21:29:32 -0300

    restayling cards

--- js/script.js
+++ js/script.js
@@ -9,3 +9,36 @@ navLinkEls.forEach(navLinkEl => {
         navLinkEl.classList.add('active');
     }
 });
+
+
+//Efecto Fade-in
+const appearOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -100px 0px"
+};
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
+
+const appearOnScroll = new IntersectionObserver(
+    function(
+        entries, 
+        appearOnScroll
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            }else{
+                entry.target.classList.add('appear');
+                appearOnScroll.unobserve(entry.target);
+            };
+        });
+},
+appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+})
